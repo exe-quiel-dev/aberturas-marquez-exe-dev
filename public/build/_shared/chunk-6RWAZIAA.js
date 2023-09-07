c78dd2597119d408082a9a7e3e377536baec87fd
exe-quiel-dev <exe.quiel.developer@outlook.com>
2023-09-07 11:07:47 -0300

    Ready for Deploy

--- public/build/_shared/chunk-6RWAZIAA.js
+++
@@ -1,7 +0,0 @@
-// public/imgs/puerta-placa-pino-marco-pino-finger-jo.webp
-var puerta_placa_pino_marco_pino_finger_jo_default = "/build/_assets/puerta-placa-pino-marco-pino-finger-jo-UV4T25IT.webp";
-
-export {
-  puerta_placa_pino_marco_pino_finger_jo_default
-};
-//# sourceMappingURL=/build/_shared/chunk-6RWAZIAA.js.map
---
+++ public/build/_shared/chunk-P6CSIOGY.js
@@ -0,0 +1,1 @@
+var h="/build/_assets/puerta-placa-pino-marco-pino-finger-jo-UV4T25IT.webp";export{h as a};
